d604921f7fab62acd8aed60822a355665ebe630e
Chandali-Dasanayaka <chandali2000111@gmail.com>
2025-05-28 04:36:59 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<?php
+// footer.php - Simple reusable footer for all pages
+?>
+<footer style="background-color: #333; color: #fff; text-align: center; padding: 1em 0; position: relative; bottom: 0; width: 100%;">
+    <p>&copy; <?php echo date("Y"); ?> Your Website Name. All rights reserved.</p>
+</footer>
---
+++ navbar.php
@@ -0,0 +1,12 @@
+<?php
+// navbar.php - Simple reusable navigation bar for all pages
+?>
+<nav style="background-color: #333; padding: 1em;">
+    <ul style="list-style: none; margin: 0; padding: 0; display: flex;">
+        <li style="margin-right: 20px;"><a href="index.php" style="color: #fff; text-decoration: none;">Home</a></li>
+        <li style="margin-right: 20px;"><a href="about.php" style="color: #fff; text-decoration: none;">About</a></li>
+        <li style="margin-right: 20px;"><a href="services.php" style="color: #fff; text-decoration: none;">Services</a></li>
+        <li style="margin-right: 20px;"><a href="contact.php" style="color: #fff; text-decoration: none;">Contact</a></li>
+        <!-- Add more links as needed -->
+    </ul>
+</nav>
